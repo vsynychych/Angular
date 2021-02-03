b5e905cd04bd21145802ed500c07fc3707404319
vsynychych <vsynychych@gmail.com>
2021-02-04 01:41:28 +0200

    Lesson5_Forms_Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularone';
+  title = 'Angular_Lesson5_Forms_Validators';
+  users: User [] = [];
+
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  age = new FormControl('', [Validators.required, Validators.min(16), Validators.max(99)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  myGroup = new FormGroup({
+      name: this.name,
+      age: this.age,
+      email: this.email
+    }
+  );
+
+  action(): void {
+    console.log(this.myGroup);
+  }
+
+  addUser(): void {
+    this.users.push({
+        name: this.name.value,
+        age: this.age.value,
+        email: this.email.value
+      }
+    );
+  }
+}
+
+class User {
+  name: string;
+  age: number;
+  email: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
